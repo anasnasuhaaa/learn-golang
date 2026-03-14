9d1d50b9df420b7131a38b643feb8c62562dac48
Anas Nasuha <infoanaspedia@gmail.com>
2026-03-15 06:36:36 +0700

    update arr

--- array/arr.go
+++ array/arr.go
@@ -5,25 +5,58 @@ import (
 )
 
 func main() {
+
+	// Array Colors
 	var myColors [4]string
 	myColors[0] = "red"
 	myColors[1] = "green"
 	myColors[2] = "blue"
 	myColors[3] = "black"
 
-	for i := 0; i < len(myColors); i++ {
-		fmt.Println(i+1, myColors[i])
+	// Array Days
+	days := [7]string{
+		"Senin",
+		"Selasa",
+		"Rabu",
+		"Kamis",
+		"Jumat",
+		"Sabtu",
+		"Minggu",
 	}
 
+	// Create New mergeArr for saving value of myColors + days
+	var mergeArr []string
+
+	// Append myColors value to mergeArr
+	for i := 0; i < len(myColors); i++ {
+		mergeArr = append(mergeArr, myColors[i])
+	}
+
+	// Append days value to mergeArr
+	for i := 0; i < len(days); i++ {
+		mergeArr = append(mergeArr, days[i])
+	}
+
+	//Display mergeArr
+	for j := 0; j < len(mergeArr); j++ {
+		fmt.Println(j+1, mergeArr[j])
+	}
+
+	// User choice
 	var userChoiceNumber int = 0
 	fmt.Print("Pilih angka: ")
 	fmt.Scan(&userChoiceNumber)
 
-	for i := 0; i < len(myColors); i++ {
+	// Display user choice
+	for i := 0; i < len(mergeArr); i++ {
 		if userChoiceNumber == i {
 			fmt.Print("Your Coice is: ")
-			fmt.Print(myColors[i-1])
+			fmt.Print(mergeArr[i-1])
+		} else if userChoiceNumber > (len(mergeArr)-1) || userChoiceNumber < 1 {
+			fmt.Print("Pilihan tidak tersedia")
+			break
 		}
+
 	}
 
 }
